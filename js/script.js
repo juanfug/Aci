608f5360f58340b24021fed4b163fdb691be6a33
juanfug <54547859+juanfug@users.noreply.github.com>
2022-02-01 12:24:46 -0500

    funcion mostrar navvar diagonal

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+
+menu = document.getElementById("header");
+nav = document.getElementById("nav");
+body = document.getElementById("container_all");
+
+
+function mostrar_menu() {
+    menu.classList.toggle('move_content');
+    body.classList.toggle('move_content');
+    nav.classList.toggle('move_nav');
+}
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 760) {
+        menu.classList.remove('move_content');
+        body.classList.remove('move_content');
+        nav.classList.remove('move_nav');
+    }
+})
